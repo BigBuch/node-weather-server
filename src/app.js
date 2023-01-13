d4c64122d7552e509c9f1db5240db25d90e8c0fb
rostyslav.bulanov <rostyslav.bulanov@gmail.com>
2023-01-13 14:43:48 +0100

    heroku config

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require("./utils/geocode.js");
 const getWeather = require("./utils/forecast.js");
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 // Define directory paths
 const publicDirectory = path.join(__dirname, "../public");
 const templatesPath = path.join(__dirname, "../templates/views");
@@ -83,6 +83,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on 3000");
+app.listen(port, () => {
+  console.log("Server is up on " + port);
 });
